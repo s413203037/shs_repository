0045f780d121745992f9d30fb02d0ce4f937b68a
s413203037 <413203037@qq.com>
2021-06-15 10:40:52 +0800

    注解练习

---
+++ nx_study/src/main/java/com/shs/nxstudy/no3_annotation/test1_Import/Cat.java
@@ -0,0 +1,14 @@
+package com.shs.nxstudy.no3_annotation.test1_Import;
+
+/**
+ * ClassName: Cat <br/>
+ * Description: <br/>
+ * date: 2021/5/27 17:22<br/>
+ *
+ * @author Blue_sky<br />
+ */
+public class Cat {
+    public Cat(){
+        System.out.println("这是Cat类");
+    }
+}
