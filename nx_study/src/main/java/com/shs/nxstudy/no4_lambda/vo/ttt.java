44f187c4cb4602a7d0ff085ada136e23f3410c39
s413203037 <413203037@qq.com>
2021-06-15 11:38:27 +0800

    注解

---
+++ nx_study/src/main/java/com/shs/nxstudy/no4_lambda/vo/ttt.java
@@ -0,0 +1,11 @@
+package com.shs.nxstudy.no4_lambda.vo;
+
+/**
+ * ClassName: ttt <br/>
+ * Description: <br/>
+ * date: 2021/6/15 11:37<br/>
+ *
+ * @author Blue_sky<br />
+ */
+public class ttt {
+}
